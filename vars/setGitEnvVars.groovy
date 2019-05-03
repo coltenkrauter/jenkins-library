0bac1b54e771e77c9126ff10d93eccd50ff3554d
Colten Krauter <ckrauter@starbucks.com>
2019-05-03 16:32:20 -0700

    Update setGitEnvVars.groovy

--- vars/setGitEnvVars.groovy
+++ vars/setGitEnvVars.groovy
@@ -4,9 +4,9 @@ def call() {
     def gitHub = new GitHub(this);
     def commitVars = checkout scm;
 
-    pipeline.env.GITHUB_API_URL = "https://api.github.com";
-    pipeline.env.GITHUB_TOKEN_CRED_ID = "build-server-github-integration";
-    pipeline.env.GITHUB_URL = "https://github.com";
+    env.GITHUB_API_URL = "https://api.github.com";
+    env.GITHUB_TOKEN_CRED_ID = "build-server-github-integration";
+    env.GITHUB_URL = "https://github.com";
 
     withCredentials([string(credentialsId: env.GITHUB_TOKEN_CRED_ID, variable: 'TOKEN')]) {
     	gitHub.setEnvVars(commitVars, TOKEN);
