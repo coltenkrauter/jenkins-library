8c7e14beaeb6d979fcd71ada014f3c80a8c29f3e
Colten Krauter <ckrauter@starbucks.com>
2019-05-03 15:51:13 -0700

    Update notifyBuildEnd.groovy

--- vars/notifyBuildEnd.groovy
+++ vars/notifyBuildEnd.groovy
@@ -1,6 +1,33 @@
 import components.Slack
 
-def call() {
+def call(STAGENAME) {
     def slack = new Slack(this);
-    slack.echo("Heyyy");
+    DURATION = slack.get_duration_string(new Date(env.BUILD_START), new Date());
+
+    color = "danger";
+    message = "Build <${env.RUN_DISPLAY_URL}|#${env.BUILD_NUMBER}> failed";
+    if (env.SUCCESS) {
+    	color = "good";
+        message = "Build <${env.RUN_DISPLAY_URL}|#${env.BUILD_NUMBER}> finished successfully";
+    }
+
+    attachment = [
+        [
+            color: color,
+            fields: [
+                [
+                    title: "Message",
+                    value: message
+                ],
+                [
+                    title: "Duration",
+                    value: DURATION
+                ]
+            ],
+        ]
+    ];
+
+    slack.postAttachment(env.BUILD_LOG_SLACK_THREAD, attachment);
+
+    return new Date();
 }
