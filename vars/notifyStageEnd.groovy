dbe2ba1b09825bad36f5cd9391b0a5a87ae671f4
Colten Krauter <ckrauter@starbucks.com>
2019-05-03 15:43:08 -0700

    Update notifyStageEnd.groovy

--- vars/notifyStageEnd.groovy
+++ vars/notifyStageEnd.groovy
@@ -1,6 +1,9 @@
 import components.Slack
 
-def call() {
+def call(STAGENAME, START_DATE, END_DATE) {
     def slack = new Slack(this);
-    slack.echo("Heyyy");
+    DURATION = slack.get_duration_string(START_DATE, END_DATE);
+    slack.postMessage(env.BUILD_LOG_SLACK_THREAD, "Stage \"${STAGENAME}\" finished in ${DURATION}");
+
+    return new Date();
 }
