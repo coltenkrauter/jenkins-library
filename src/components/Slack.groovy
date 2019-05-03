60ebfcb2ffae6c8dc139e15fd0d251a0d523105b
Colten Krauter <ckrauter@starbucks.com>
2019-05-03 10:26:44 -0700

    Update Slack.groovy

--- src/components/Slack.groovy
+++ src/components/Slack.groovy
@@ -4,15 +4,33 @@ import groovy.json.JsonOutput;
 import java.time.Instant;
 
 class Slack {
-	def pipeline;
+    def pipeline;
+    def debug = true;
 
-	// Constructor
+    // Constructor
     Slack(pipeline) {
-    	this.pipeline = pipeline;
-    	pipeline.echo("CONSTRUCTOR");
+        this.pipeline = pipeline;
+
+        echo("In the Constructor");
+        
+        if (!pipeline.env.SLACK_CONSTRUCTOR_WAS_INITIALIZED) {
+            echo("FOR THE FIRST TIME");
+        } else {
+            pipeline.env.SLACK_CONSTRUCTOR_WAS_INITIALIZED = "true";            
+            echo("ALL THE REST");
+        }
+    }
+    
+    def echo(String message) {
+        pipeline.echo(message);
     }
 
-	def test() {
-		pipeline.echo("This is the SLACK CLASS");
-	}
+    def debug(String message) {
+        if (debug) {
+            echo(message);
+        }
+    }
+
+
+    
 }
