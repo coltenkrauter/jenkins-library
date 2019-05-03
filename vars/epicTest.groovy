16a45341953d7747ac0db1fa49ded6934d7bdb9d
Colten Krauter <ckrauter@starbucks.com>
2019-05-03 08:12:05 -0700

    Update epicTest.groovy

--- vars/epicTest.groovy
+++ vars/epicTest.groovy
@@ -1,16 +1,14 @@
 //import components.Slack
 
 def call() {
+    def print(message) {
+        debug = true;
 
+        if (debug) {
+            echo(message);
+        }
+    }
     echo "I am the most epic test everrrrr"
     print("tessssssssssssssssssssssssssssssssssssssst");
     echo "yah"
-}
-
-def print(message) {
-    debug = true;
-
-    if (debug) {
-        echo(message);
-    }
 }
