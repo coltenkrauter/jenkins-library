47207cc21614881402ee43a80d1547373679053c
Colten Krauter <ckrauter@starbucks.com>
2019-05-03 11:50:07 -0700

    Update Slack.groovy

--- src/components/Slack.groovy
+++ src/components/Slack.groovy
@@ -18,9 +18,9 @@ class Slack {
             pipeline.env.BUILD_LOG_SLACK_THREAD = "";
             pipeline.env.SUCCESS = "true";
 
-            build_user = env.GIT_COMMITTER_NAME;
+            build_user = pipeline.env.GIT_COMMITTER_NAME;
             pipeline.wrap([$class: 'BuildUser']) {
-                if (pipeline.env.BUILD_USER) {
+                if (env.BUILD_USER) {
                     build_user = BUILD_USER;
                 }
             }
