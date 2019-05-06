edacde4ecdec465855f6bb986a791fb1f0f91106
Colten Krauter <coltenkrauter@gmail.com>
2019-05-06 15:24:04 -0700

    Test markdown

--- vars/notifyStageEnd.groovy
+++ vars/notifyStageEnd.groovy
@@ -3,7 +3,7 @@ import components.Slack
 def call(STAGENAME, START_DATE, END_DATE) {
     def slack = new Slack(this);
     DURATION = slack.get_duration_string(START_DATE, END_DATE);
-    slack.postMessage(env.BUILD_LOG_SLACK_THREAD, "Stage \"${STAGENAME}\" finished in ${DURATION}");
+    slack.postMessage(env.BUILD_LOG_SLACK_THREAD, "Stage *${STAGENAME}* finished in ${DURATION}");
 
     return new Date();
 }
