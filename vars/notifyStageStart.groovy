f7a5cbceacc9135e2142e3a6e586dd7bd970621f
Colten Krauter <coltenkrauter@gmail.com>
2019-05-06 15:31:53 -0700

    Update stage message

--- vars/notifyStageStart.groovy
+++ vars/notifyStageStart.groovy
@@ -3,7 +3,7 @@ import components.Slack
 def call(STAGENAME) {
     def slack = new Slack(this);
     
-    slack.postMessage(env.BUILD_LOG_SLACK_THREAD, "Stage \"${STAGENAME}\" started");
+    slack.postMessage(env.BUILD_LOG_SLACK_THREAD, "*[${STAGENAME}]* started");
 
     return new Date();
 }
