40ab2ebb54db20f97c8eb00e9d08b1924b73f538
Colten Krauter <ckrauter@starbucks.com>
2019-05-07 11:15:47 -0700

    Updates

--- vars/notifyBuildEnd.groovy
+++ vars/notifyBuildEnd.groovy
@@ -5,11 +5,13 @@ def call() {
     DURATION = slack.get_duration_string(new Date(env.BUILD_START), new Date());
 
     color = "danger";
-    message = "Build <${env.RUN_DISPLAY_URL}|#${env.BUILD_NUMBER}> failed";
-    
+    slackMessage = "Build <${env.RUN_DISPLAY_URL}|#${env.BUILD_NUMBER}> failed";
+    logMessage = "Build ${env.BUILD_NUMBER} failed";
+
     if (env.SUCCESS && env.SUCCESS == "true") {
-    	color = "good";
-        message = "Build <${env.RUN_DISPLAY_URL}|#${env.BUILD_NUMBER}> finished successfully";
+        color = "good";
+        slackMessage = "Build <${env.RUN_DISPLAY_URL}|#${env.BUILD_NUMBER}> finished successfully";
+        logMessage = "Build ${env.BUILD_NUMBER} finished successfully";
     }
 
     attachment = [
@@ -18,7 +20,7 @@ def call() {
             fields: [
                 [
                     title: "Message",
-                    value: message
+                    value: slackMessage
                 ],
                 [
                     title: "Duration",
@@ -29,6 +31,7 @@ def call() {
     ];
 
     slack.postAttachment(env.BUILD_LOG_SLACK_THREAD, attachment);
+    echo(logMessage);
 
     return new Date();
 }
