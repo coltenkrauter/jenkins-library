1c5a277d67a98e1720f7b82b34d3329cbd0be19a
Colten Krauter <ckrauter@starbucks.com>
2019-05-07 13:50:16 -0700

    Update notifyError.groovy

--- vars/notifyError.groovy
+++ vars/notifyError.groovy
@@ -1,6 +1,13 @@
 import components.Slack
 
-def call() {
+def call(err) {
+    env.SUCCESS = "false";
+
+    /* Truncate error message to 100 chars*/
+    errMessage = err.toString();
+    if (errMessage.length() > 100)
+        errMessage = errMessage.take(100) + "...";
+
     if (env.GIT_REPO_NAME) {
         def slack = new Slack(this);
 
@@ -9,7 +16,14 @@ def call() {
                 color: "danger",
                 fields: [
                     [
-                        value: "@here, *<${env.GIT_REPO_URL}|${env.GIT_REPO_NAME}>/<${env.GIT_BRANCH_URL}|${env.GIT_BRANCH_NAME}>* - build <${env.RUN_DISPLAY_URL}|#${env.BUILD_NUMBER}> failed :face_with_monocle:"
+                        title: "Message",
+                        value: "@here, *<${env.GIT_REPO_URL}|${env.GIT_REPO_NAME}>/<${env.GIT_BRANCH_URL}|${env.GIT_BRANCH_NAME}>* - <${env.RUN_DISPLAY_URL}|build #${env.BUILD_NUMBER}> failed :face_with_monocle:",
+                        short: true
+                    ],
+                    [
+                        title: "Error",
+                        value: errMessage,
+                        short: true
                     ]
                 ],
                 markdown: ["pretext"]
@@ -29,9 +43,12 @@ def call() {
 
         // Post message in Slack thread and broadcast to channel
         slack.postAttachment(env.BUILD_LOG_SLACK_THREAD, attachment);
-        } else {
-            slackSend(message: "@here, build <${env.RUN_DISPLAY_URL}|#${env.BUILD_NUMBER}> failed :face_with_monocle:", color: "danger");
-        }
+    } else {
+        slackSend(message: "@here, build <${env.RUN_DISPLAY_URL}|#${env.BUILD_NUMBER}> failed :face_with_monocle:", color: "danger");
+    }
+
+    echo("Pipeline Failed: ${err}");
+    throw(err);
 
     // Return build start date
     return new Date(env.BUILD_START);
