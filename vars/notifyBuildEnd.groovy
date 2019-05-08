e822a69fc982900dd79758ea6ae848683a7274b2
Colten Krauter <ckrauter@starbucks.com>
2019-05-08 12:55:40 -0700

    Update notifyBuildEnd.groovy

--- vars/notifyBuildEnd.groovy
+++ vars/notifyBuildEnd.groovy
@@ -11,7 +11,7 @@ def call() {
 
     if (env.SUCCESS && env.SUCCESS == "true") {
         /* Green */
-        color = "#44bd32";
+        color = "#2ecc71";
         slackMessage = "Build <${env.RUN_DISPLAY_URL}|#${env.BUILD_NUMBER}> finished successfully";
         logMessage = "Build ${env.BUILD_NUMBER} finished successfully";
     }
