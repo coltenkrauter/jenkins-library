26f8592dd246f460b36136377cb22e69cd4a0a66
Colten Krauter <ckrauter@starbucks.com>
2019-05-08 12:05:44 -0700

    Update notifyBuildEnd.groovy

--- vars/notifyBuildEnd.groovy
+++ vars/notifyBuildEnd.groovy
@@ -4,12 +4,14 @@ def call() {
     def slack = new Slack(this);
     DURATION = slack.getDurationString(new Date(env.BUILD_START), new Date());
 
-    color = "danger";
+    /* Red */
+    color = "#e84118";
     slackMessage = "Build <${env.RUN_DISPLAY_URL}|#${env.BUILD_NUMBER}> failed";
     logMessage = "Build ${env.BUILD_NUMBER} failed";
 
     if (env.SUCCESS && env.SUCCESS == "true") {
-        color = "good";
+        /* Green */
+        color = "#44bd32";
         slackMessage = "Build <${env.RUN_DISPLAY_URL}|#${env.BUILD_NUMBER}> finished successfully";
         logMessage = "Build ${env.BUILD_NUMBER} finished successfully";
     }
