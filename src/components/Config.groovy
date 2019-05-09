90036b2a23bba9a612633c25784361ab59db0fec
Colten Krauter <ckrauter@starbucks.com>
2019-05-09 09:10:54 -0700

    Add comma

--- src/components/Config.groovy
+++ src/components/Config.groovy
@@ -2,7 +2,7 @@ package components;
 
 class Config {
 	def CONFIGS = [
-		SLACK_CHANNEL: "build-log"
+		SLACK_CHANNEL: "build-log",
 		GITHUB_API_URL: "https://api.github.com",
     	GITHUB_TOKEN_CRED_ID: "build-server-github-integration",
     	GITHUB_URL: "https://github.com"
