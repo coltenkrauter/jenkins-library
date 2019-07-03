2f7d4035d3e143e0c00f23b35ba9d12dd3b36c57
Colten Krauter <coltenkrauter@gmail.com>
2019-07-02 21:12:21 -0700

    Delete playbook.groovy

--- vars/playbook.groovy
+++
@@ -1,9 +0,0 @@
-def call(ANSIBLE_PLAYBOOK, INVENTORY, EXTRA_VARS) {
-    ansiblePlaybook(
-        playbook: "${ANSIBLE_REPO}/${ANSIBLE_PLAYBOOK}.yaml",
-        inventory: "${ANSIBLE_REPO}/inventories/${INVENTORY}/hosts",
-        extras: "--extra-vars \"${EXTRA_VARS}\"",
-        credentialsId: "ssh-key-ubuntu",
-        vaultCredentialsId:  "ansible-vault-password",
-    );
-}
